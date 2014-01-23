cf8e05456a208f540c88c85c50983c5420e7fabc
toyama satoshi <toyamarinyon@gmail.com>
2014-01-24 06:09:12 +0900

    build login form

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -1,5 +1,17 @@
 'use strict';
 
-angular.module('mean.system').controller('IndexController', ['$scope', 'Global', function ($scope, Global) {
+angular.module('mean.system').controller('IndexController', ['$scope', 'Global', '$window', '$http', '$route', function ($scope, Global, $window, $http, $route) {
     $scope.global = Global;
+    $scope.guestLogin = function () {
+        if ( ($window.mockWindow || $window).confirm('facebookログインすると、これからの記録をずっと残すことができます。\nそれでもゲストログインになさいますか？') ) {
+            $http.post('/users', {'name':'アップル','email':'dummy@tamurine.jp','password':'123456'})
+                .success(function () {
+                    ($window.mockWindow || $window).alert('[ゲスト]アップルさんでログインします。');
+                    $scope.global = Global;
+                    $route.reload();
+                })
+                .error(function () {
+                });
+        }
+    };
 }]);
