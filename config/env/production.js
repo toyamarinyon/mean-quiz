236f9702da5bb32fc933797566dbbce87894026a
toyama satoshi <toyamarinyon@gmail.com>
2014-01-24 01:36:12 +0000

    update production

--- config/env/production.js
+++ config/env/production.js
@@ -1,28 +1,29 @@
 'use strict';
 
+var port = process.env.PORT;
 module.exports = {
-    db: "mongodb://localhost/mean",
+    db: "mongodb://localhost/mean-dev",
     app: {
-        name: "MEAN - A Modern Stack - Production"
+        name: "クイズ Bプロジェクト!"
     },
     facebook: {
-        clientID: "APP_ID",
-        clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/facebook/callback"
+        clientID: process.env.MEAN_QUIZ_FACEBOOK_CLIENT_ID || "APP_ID",
+        clientSecret: process.env.MEAN_QUIZ_FACEBOOK_APP_SECRET || "APP_SECRET",
+        callbackURL: "/auth/facebook/callback"
     },
     twitter: {
         clientID: "CONSUMER_KEY",
         clientSecret: "CONSUMER_SECRET",
-        callbackURL: "http://localhost:3000/auth/twitter/callback"
+        callbackURL: "/auth/twitter/callback"
     },
     github: {
         clientID: "APP_ID",
         clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/github/callback"
+        callbackURL: "/auth/github/callback"
     },
     google: {
         clientID: "APP_ID",
         clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/google/callback"
+        callbackURL: "/auth/google/callback"
     }
 }
