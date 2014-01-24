95eac911a934a2f4137961e164b6d19135801d53
toyama satoshi <toyamarinyon@gmail.com>
2014-01-24 01:30:52 +0000

    update config

--- config/env/development.js
+++ config/env/development.js
@@ -2,27 +2,28 @@
 
 module.exports = {
     db: "mongodb://localhost/mean-dev",
+	  port: process.env.PORT || 3000,
     app: {
         name: "MEAN - A Modern Stack - Development"
     },
     facebook: {
         clientID: process.env.MEAN_QUIZ_FACEBOOK_CLIENT_ID || "APP_ID",
         clientSecret: process.env.MEAN_QUIZ_FACEBOOK_APP_SECRET || "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/facebook/callback"
+        callbackURL: "http://localhost:"+port+"/auth/facebook/callback"
     },
     twitter: {
         clientID: "CONSUMER_KEY",
         clientSecret: "CONSUMER_SECRET",
-        callbackURL: "http://localhost:3000/auth/twitter/callback"
+        callbackURL: "http://localhost:"+port+"/auth/twitter/callback"
     },
     github: {
         clientID: "APP_ID",
         clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/github/callback"
+        callbackURL: "http://localhost:"+port+"/auth/github/callback"
     },
     google: {
         clientID: "APP_ID",
         clientSecret: "APP_SECRET",
-        callbackURL: "http://localhost:3000/auth/google/callback"
+        callbackURL: "http://localhost:"+port+"/auth/google/callback"
     }
 }
