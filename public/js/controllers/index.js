0e1a046f83f6192c52a35b7b30706cd25ebb5eca
toyama satoshi <toyamarinyon@gmail.com>
2014-01-24 04:29:45 +0000

    update guest

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -3,7 +3,6 @@
 angular.module('mean.system').controller('IndexController', ['$scope', 'Global', '$window', '$http', '$route', function ($scope, Global, $window, $http, $route) {
     $scope.global = Global;
     $scope.guestLogin = function () {
-        $route.reload();
         if ( ($window.mockWindow || $window).confirm('facebookログインすると、これからの記録をずっと残すことができます。\nそれでもゲストログインしますか？') ) {
             $http.get('/guest')
                 .success(function (guest_user) {
@@ -11,6 +10,7 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
                         .success(function () {
                             $http.post('/guest/use');
                             ($window.mockWindow || $window).alert('ゲストでログインします。\n明日はfacebookログインしてみてね。');
+                            $http.reload();
                         })
                         .error(function () {
                         });
