e2ff6586a51366c8171819c1c79418179f683761
toyama satoshi <toyamarinyon@gmail.com>
2014-01-30 11:44:38 +0000

    question

--- public/js/controllers/question.js
+++ public/js/controllers/question.js
@@ -1,9 +1,9 @@
 'use strict';
 
-angular.module('mean').controller('QuestionController', ['$scope', '$window', '$http', '$location', '$rootParam', function ($scope, $window, $http, $location, $rootParam) {
-  $http.get('/question/'+$rootParam.questionNo)
+angular.module('mean').controller('QuestionController', ['$scope', '$window', '$http', '$location', '$routeParams', function ($scope, $window, $http, $location, $routeParams) {
+  $http.get('/question/'+$routeParams.questionNo)
     .success(function(data) {
-      $scope.choices = data.choices;
+      $scope.question = data;
     })
     .error(function() {
     });
